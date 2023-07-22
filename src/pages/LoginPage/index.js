7d2713cffa329d09f8589bf4d2bb7edc5b68db5e
ruanlsdn <ruanlsdn@gmail.com>
2023-07-22 01:23:07 -0300

    a

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -51,7 +51,7 @@ export default function Login() {
           source={require("../../assets/logo.png")}
           resizeMode="contain"
         />
-        <Text style={styles.titleLogo}>Budget</Text>
+        <Text style={styles.titleLogo}>Orçamentos</Text>
         <Text style={styles.descriptionLogo}>
           A organização do seu negócio em suas mãos!
         </Text>
